74f69c1cad04261a14bae41c78d448306910b3b9
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-02-10 21:45:22 +0100

    feat: add dependency injection (io.Writer)

---
+++ lgwt/08_dep_injection/dep.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+}
