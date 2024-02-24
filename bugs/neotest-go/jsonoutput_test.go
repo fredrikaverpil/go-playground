e5f851b92506b1ad294ec6c3be44492a36e3056d
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-02-24 16:50:54 +0100

    chore(bug): json output shows

--- bugs/neotest-go/jsonoutput_test.go
+++ bugs/neotest-go/jsonoutput_test.go
@@ -17,3 +17,24 @@ func Test_Level_1(t *testing.T) {
 		})
 	})
 }
+
+type Skipper struct {
+	name string
+	skip bool
+}
+
+func Test_Level_1_Skipper(t *testing.T) {
+	for _, c := range []Skipper{{name: "Level 2a", skip: false}, {name: "Level 2b", skip: true}} {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			if c.skip {
+				t.Skip()
+			}
+			t.Run("Level 3", func(t *testing.T) {
+				if c.skip {
+					t.Skip()
+				}
+			})
+		})
+	}
+}
