7878d55977792675b9aa93a88e00cff28a9554cc
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-03-08 21:18:58 +0100

    fix(sse): use http.Server with ReadHeaderTimeout instead of http.ListenAndServe

--- sse/cmd/serverv1/main.go
+++ sse/cmd/serverv1/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/events", sseHandler)
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	server := &http.Server{
+		Addr:              ":8080",
+		ReadHeaderTimeout: 10 * time.Second,
+	}
+	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("error starting server: %s\n", err)
 	}
 }
